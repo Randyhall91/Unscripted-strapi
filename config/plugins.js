fee2041cc246ebcf5611c82c04c5012583a701db
Randyhall91 <randyrocks11@gmail.com>
2022-12-23 16:26:22 -0700

    change aws names

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          accessKeyId: env('AWSS_ACCESS_KEY_ID'),
           secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          region: env('AWSS_REGION'),
           params: {
               Bucket: env('AWS_BUCKET_NAME'),
           },
