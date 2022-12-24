63cb21403b7e7307cdf3085dbd5263a14f39e2cf
Randyhall91 <randyrocks11@gmail.com>
2022-12-23 20:58:45 -0700

    change provider to work with ec2

--- config/plugins.js
+++ config/plugins.js
@@ -11,18 +11,14 @@ module.exports = ({ env }) => ({
           },
         },
         actionOptions: {
-          upload: {},
-          uploadStream: {},
+          upload: {
+            ACL: null
+          },
+          uploadStream: {
+            ACL: null
+          },
           delete: {},
         },
-      },
-      "vercel-deploy": {
-        enabled: true,
-        deployHook: env('VERCEL_DEPLOY_PLUGIN_HOOK'),
-        apiToken: env('VERCEL_DEPLOY_PLUGIN_API_TOKEN'),
-        appFilter: env('VERCEL_DEPLOY_PLUGIN_APP_FILTER'),
-        teamFilter: env('VERCEL_DEPLOY_PLUGIN_TEAM_FILTER'),
-        roles: ["strapi-super-admin"],
       },
     }
   });
